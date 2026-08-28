1c37a8d1e2b617b9f049cb22f09dbb987b8b0780
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-08-28 14:48:51 +0530

    Solved One more question in string

--- Strings/SlidingWindow/KDistinctCharacters.java
+++ Strings/SlidingWindow/KDistinctCharacters.java
@@ -24,9 +24,30 @@ public class KDistinctCharacters {
         System.out.println("Longest Substring with K Distinct Character:" + maxLen);
     }
 
+    public static void findLongestSubstringExactlyKDistinctChar(String s, int k) {
+        int left = 0, right = 0;
+        int maxLen = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            while (map.size() > k) {
+                char charAtLeft = s.charAt(left);
+                map.put(charAtLeft, map.get(charAtLeft) - 1);
+                if (map.get(charAtLeft) == 0) {
+                    map.remove(charAtLeft);
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+        System.out.println("Longest Substring with Exactly K Distinct Character:" + maxLen);
+    }
+
     public static void main(String[] args) {
-        String s = "eceba";
-        int k = 2;
-        findLongestSubstringKDistinctChar(s, k);
+        String s = "aabacbebebe";
+        int k = 3;
+        findLongestSubstringExactlyKDistinctChar(s, k);
     }
 }
