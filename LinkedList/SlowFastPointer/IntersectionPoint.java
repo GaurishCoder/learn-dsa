026a1dcd164d54bfb4d65cd84279e668b92eda95
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-09-02 17:38:41 +0530

    Solved one question on linked list

--- LinkedList/SlowFastPointer/IntersectionPoint.java
+++ LinkedList/SlowFastPointer/IntersectionPoint.java
@@ -1,6 +1,6 @@
 package LinkedList.SlowFastPointer;
 
-public class NthNodeFromEnd {
+public class IntersectionPoint {
     public static class Node {
         int data;
         Node next;
@@ -19,35 +19,40 @@ public class NthNodeFromEnd {
         System.out.println();
     }
 
-    public static void findNthNodeFromEnd(Node head,int n) {
-        Node slow=head;
-        Node fast=head;
+    public static Node findIntersectionPoint(Node headA,Node headB) {
+        Node p1 = headA;
+        Node p2 = headB;
 
-        // create gap between two pointer
-        for (int i = 0; i < n; i++) {
-            fast = fast.next;
+        while(p1!=p2){
+            if (p1==null) {
+                p1=headB;
+            }else{
+                p1=p1.next;
+            }
+
+            if (p2==null) {
+                p2=headA;
+            }else{
+                p2=p2.next;
+            }
         }
-        while (fast!=null) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        System.out.println("Nth Node from end is:"+slow.data);
+        return p1;
     }
-   
 
+    
+       
     public static void main(String[] args) {
         Node head = new Node(1);
         Node b = new Node(2);
         Node c = new Node(3);
         Node d = new Node(4);
-        Node e = new Node(5);
+        // Node e = new Node(5);
 
         head.next = b;
         b.next = c;
         c.next = d;
         d.next = c;
-        
-        displayList(head);
-        findNthNodeFromEnd(head, 2);
+       
+   
     }
 }
