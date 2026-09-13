c58ddfa939d7b5da37652277139ab9e7e1a0129c
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-09-13 14:45:14 +0530

    Added Notes of Deque

--- Queue/Deque/DequeFramework.java
+++ Queue/Deque/DequeFramework.java
@@ -1,6 +1,14 @@
 package Queue.Deque;
 import java.util.*;
 
+
+/**
+ * Deque
+ *  Double Ended Queue.
+ *  It has two ways operation of insertion and deletion.
+ *  Queue that performs remove first and remove last as well as add first and add last
+ */
+
 public class DequeFramework {
     public static void main(String[] args) {
         Deque<Integer> dq = new LinkedList<>();
