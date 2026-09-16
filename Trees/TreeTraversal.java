7b6e9cbd47d7740c3b912780ec441076d5b25c84
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-09-16 23:05:23 +0530

    Learn about traversal in tree

---
+++ Trees/TreeTraversal.java
@@ -0,0 +1,60 @@
+package Trees;
+
+public class TreeTraversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println("Preorder:");
+        tree.preorder(root);
+        System.out.println("\nInorder:");
+        tree.inorder(root);
+        System.out.println("\nPostorder:");
+        tree.postorder(root);
+    }
+}
