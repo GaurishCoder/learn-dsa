46a0b77029a6e98e376925de281b1bf6b221e968
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-09-19 18:24:59 +0530

    Solved some question in trees

---
+++ Trees/HeightofTree.java
@@ -0,0 +1,49 @@
+package Trees;
+
+public class HeightofTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static int count(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = count(root.left);
+        int rh = count(root.right);
+        return lh + rh + 1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        // height of tree
+        System.out.println("Height of Tree is:" + height(root));
+
+        // total number of nodes
+        System.out.println("Total number of nodes is:" + count(root));
+    }
+}
