8f6b971304493a3410af8735a3a337eacd53773b
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-09-20 19:14:14 +0530

    Implemented BST

--- BinarySearchTree/BuildBST.java
+++ BinarySearchTree/BuildBST.java
@@ -1,6 +1,6 @@
 package BinarySearchTree;
 
-public class Basics {
+public class BuildBST {
     static class Node {
         int data;
         Node left;
@@ -20,38 +20,26 @@ public class Basics {
         inorder(root.right);
     }
 
-    public static Node insert(Node root,int val) {
-        if (root==null) {
+    public static Node insert(Node root, int val) {
+        if (root == null) {
             root = new Node(val);
             return root;
         }
         if (root.data > val) {
-            //left subtree
             root.left = insert(root.left, val);
-        }else{
-            //right subtree
+        } else {
             root.right = insert(root.right, val);
         }
         return root;
     }
-   
+
     public static void main(String[] args) {
 
-        /*
-         *  5
-         * / \ 
-         * 1  7
-         *  \
-         *   3
-         *  / \
-         * 2   4
-         */
-
-        int values[] = {5,1,8,4,2,7,};
+        int values[] = { 8, 5, 1, 2, 10, 7, 4, 6, 3, 9 };
         Node root = null;
 
-        for (int i = 0; i < values.length; i++) {
-            root = insert(root,values[i]);
+        for(int i=0;i<values.length;i++){
+            root = insert(root, values[i]);
         }
         inorder(root);
     }
