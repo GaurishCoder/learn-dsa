2469d52498cfd84cf78a38e7f13db5c167e13f44
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-09-22 23:41:26 +0530

    Implement DeleteNode functionality in Binary Search Tree

---
+++ BinarySearchTree/DeleteNode.java
@@ -0,0 +1,84 @@
+package BinarySearchTree;
+
+public class DeleteNode {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node createdBST() {
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+        root.left.left.left = new Node(1);
+        root.left.left.right = new Node(4);
+        root.right.right.right = new Node(14);
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+
+    }
+
+    public static Node delete(Node root, int val) {
+        if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else {
+
+            // Case 1:leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // Case 2:One child
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+
+            // Case 3: Two Child
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = createdBST();
+
+        inorder(root);
+        System.out.println();
+        delete(root, 5);
+        System.out.println();
+        inorder(root);
+
+    }
+}
